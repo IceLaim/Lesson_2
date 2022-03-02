3afef7d16aab96cdd62af719c0ebee8044239e86
IceLaim <100352879+IceLaim@users.noreply.github.com>
2022-03-02 22:35:41 +0300

    Домашнее задание 2

--- src/com/geekbrains/Lesson_2/HomeWork2.java
+++ src/com/geekbrains/Lesson_2/HomeWork2.java
@@ -1,7 +1,29 @@
 package com.geekbrains.Lesson_2;
 
 public class HomeWork2 {
+
     public static void main(String[] args) {
-        System.out.println("Hey!");
+        System.out.println(sumWithin10and20(10, 5));
+        numPositiveOrNegative(-5);
     }
-}
+
+
+    public static boolean sumWithin10and20(int a, int b) {
+        int sum = a + b;
+        return sum >= 10 & sum <= 20;
+    }
+
+
+    public static void numPositiveOrNegative(int x) {
+        if (x < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Positive");
+        }
+    }
+
+
+
+
+        }
+

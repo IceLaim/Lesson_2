8e497e873095461d74cf04dcbb9164647ed1f7f6
IceLaim <100352879+IceLaim@users.noreply.github.com>
2022-03-03 20:17:13 +0300

    Домашнее задание 2

--- src/com/geekbrains/Lesson_2/HomeWork2.java
+++ src/com/geekbrains/Lesson_2/HomeWork2.java
@@ -8,14 +8,10 @@ public class HomeWork2 {
         System.out.println(numNegative(25));
         printWordNTimes("Привет", 3 );
     }
-
-
     public static boolean sumWithin10and20(int a, int b) {
         int sum = a + b;
         return sum >= 10 && sum <= 20;
     }
-
-
     public static void numPositiveOrNegative(int с) {
         if (с < 0) {
             System.out.println("Отрицательное");
@@ -23,18 +19,13 @@ public class HomeWork2 {
             System.out.println("Положительное");
         }
     }
-
-
     public static boolean numNegative(int d) {
         if (d < 0) {
             return true;
         } else {
             return false;
         }
-
     }
-
-
     public static void printWordNTimes(String words, int times) {
         for (int i = 1; i <= times; i++) {
             System.out.println("[" + i + "]" + " " + words);
